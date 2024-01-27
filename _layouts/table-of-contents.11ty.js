964179ee13e50c1626d9bab0732e7b2ba9749b5e
michac789 <michaelac978@gmail.com>
2024-01-28 01:02:14 +0800

    fix double footer bug

--- _layouts/table-of-contents.11ty.js
+++ _layouts/table-of-contents.11ty.js
@@ -57,23 +57,7 @@ module.exports = class TableOfContents {
           ${this.pageButtons({ pagination })}
         </div>
       </section>
-      <div class="special-footer-copyright">
-        <div>
-          Copyright &copy; 2024 National Gallery Singapore. All rights reserved
-        </div>
-        <div class="special-small-screen-gone">
-          |
-        </div>
-        <div>
-        <a href="https://www.nationalgallery.sg/privacy-policy" target="_blank">Privacy Policy</a>
-        </div>
-        <div class="special-small-screen-gone">
-          |
-        </div>
-        <div>
-          <a href="https://www.nationalgallery.sg/" target="_blank">Website</a>
-        </div>
-      </div>`
+      `
       ,
       'liquid',
       data
